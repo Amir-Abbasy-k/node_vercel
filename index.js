81ca679dfb7bde27344aa1bfa206e9090555e93f
Amir-Abbasy-k <amirabbasyk@gmail.com>
2022-05-27 23:51:43 +0530

    port set

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-
+var port =  process.env.PORT || 4000
+ 
 
 app.get('/',(req, res)=>{
     // res.send({msg: "It's working well"})
@@ -12,7 +13,7 @@ app.get('/',(req, res)=>{
 })
 
 
-app.listen(4000, (err, result)=>{
+app.listen(port, (err, result)=>{
     if(err)console.log("Err "+err);
     console.log("Server Started on 4000");
 })
